b8ce53630f5b3c8f7a03b6aa0f3e0fd9b4cc2421
Игорь Лапай <igor.lapay@gmail.com>
2019-08-28 06:11:41 +0600

    FE: migrate to VueJS

--- frontend-web/src/main/resources/static/js/app.js
+++ frontend-web/src/main/resources/static/js/app.js
@@ -10,6 +10,7 @@ const URL_PATH_API = URL_ORIGIN + '/api';
 const URL_PATH_API_VK_AUTH = URL_PATH_API + '/vk/auth';
 const URL_PATH_API_VK_FORGET_ME = URL_PATH_API + '/vk/forgetMe';
 const URL_PATH_API_VK_FRIENDS = URL_PATH_API + '/vk/friends';
+const URL_PATH_API_VK_SUBSCRIBERS = URL_PATH_API + '/vk/subscribers';
 const URL_PATH_API_GEO_GEOCODE = URL_PATH_API + '/geo/geocode';
 
 const VK_URL_PATH_OAUTH = 'https://oauth.vk.com/authorize';
@@ -31,22 +32,25 @@ const store = new Vuex.Store({
         isLoading: true,
         isAuth: false,
         cities: [],
-        friendsVK: [],
-        subsVK: [],
+        people: [],
     },
-    getters: {},
+    getters: {
+        getCityById: state => id => {
+            return state.cities.find(city => city.id == id);
+        },
+    },
     mutations: {
         setIsLoading: (state, val) => state.isLoading = val,
         setIsAuth: (state, val) => state.isAuth = val,
-        setFriendsVK: (state, peoples) => state.friendsVK = peoples,
         setCities: (state, cities) => state.cities = cities,
+        setPeople: (state, people) => state.people = people,
     },
     actions: {
         fetchFriendsVK: async (context) => {
             try {
                 let response = await axios.get(URL_PATH_API_VK_FRIENDS);
                 if (response.status == 200) {
-                    context.commit('setFriendsVK', response.data);
+                    context.commit('setPeople', response.data);
                 } else {
                     console.log(response);
                 }
@@ -54,7 +58,7 @@ const store = new Vuex.Store({
                 console.log(err);
             }
         },
-        geocodeCities: async (context, cities) => {
+        updateCitiesCoords: async (context, cities) => {
             try {
                 let json = JSON.stringify(cities);
                 let response = await axios.post(URL_PATH_API_GEO_GEOCODE, json, CONTENT_TYPE_JSON);
@@ -103,15 +107,7 @@ var app = new Vue({
     data: {
         map: null,
         tileLayer: null,
-        clusterGroup: null,
-        layers: [
-            {
-                id: 0,
-                name: 'friendsVK',
-                active: false,
-                features: [],
-            },
-        ],
+        clusters: null,
     },
     mounted: function () {
 
@@ -127,7 +123,7 @@ var app = new Vue({
         this.setIsAuth(userAlreadyHaveSession);
 
         this.initMap();
-        this.initLayers();
+        this.fetchFriendsVk();
 
         this.setIsLoading(false);
     },
@@ -135,8 +131,66 @@ var app = new Vue({
         vkOAuthURI: () => VK_URL_FULL_OAUTH,
         isLoading: () => store.state.isLoading,
         isAuth: () => store.state.isAuth,
-        friendsVK: () => store.state.friendsVK,
         cities: () => store.state.cities,
+        people: () => store.state.people,
+    },
+    watch: {
+        people: function () {
+            // people updated event, lets update cities (extract from people)
+            let citiesMap = this.people.reduce((map, person) => {
+                if (person.cityId == -1) return map;
+                map[person.cityId] = {
+                    id: person.cityId,
+                    name: person.city,
+                    country: person.country,
+                    latitude: 0,
+                    longitude: 0
+                };
+                return map;
+            }, {});
+            let cities = Object.values(citiesMap);
+            store.dispatch('updateCitiesCoords', cities);
+        },
+        cities: function () {
+            // cities updated event, let update markers
+            let markers = [];
+            let personInCityCounters = [];
+            for (let person of this.people) {
+                // get city coords
+                if (person.cityId == -1) continue;
+                let city = store.getters.getCityById(person.cityId);
+                if (!city) continue;
+
+                // icon with photo
+                let markerIconWithPhoto = L.icon({
+                    iconUrl: person.photoUri,
+                    iconSize: [50, 50],
+                    popupAnchor: [0, -30]
+                });
+
+                // if 2+ person live in same place, we need offset marker
+                if (personInCityCounters[person.cityId] === undefined) {
+                    personInCityCounters[person.cityId] = 0;
+                }
+                let num = personInCityCounters[person.cityId]++;
+                let baseAngle = Math.PI * (1 + Math.sqrt(5)) * 6;
+                let r = Math.sqrt(num) * 0.001;
+                let theta = num * baseAngle;
+                let x = r * Math.cos(theta) * 0.65;
+                let y = r * Math.sin(theta);
+
+                let marker = L.marker([city.latitude + x, city.longitude + y], { icon: markerIconWithPhoto });
+                marker.bindPopup(person.firstName + ' ' + person.lastName);
+
+                markers.push(marker);
+            }
+
+            // clear old and add new markers
+            this.clusters.clearLayers();
+            for (let marker of markers) {
+                this.clusters.addLayer(marker);
+            }
+        },
     },
     methods: {
         setIsLoading: (val) => store.commit('setIsLoading', val),
@@ -144,9 +198,11 @@ var app = new Vue({
         authByCode: (code) => store.dispatch('authByCode', code),
         forgetMe: () => store.dispatch('forgetMe'),
 
+        fetchFriendsVk: () => store.dispatch('fetchFriendsVK'),
+
         initMap: function () {
             this.map = L.map('map');
-            this.map.setView([54.9884804, 73.3242361], 12);
+            this.map.setView([54.9884804, 73.3242361], 16);
 
             this.tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
                 maxZoom: 18,
@@ -155,8 +211,11 @@ var app = new Vue({
             });
             this.tileLayer.addTo(this.map)
 
-            this.clusterGroup = L.markerClusterGroup();
+            this.clusters = L.markerClusterGroup({ maxClusterRadius: 20 });
+            this.map.addLayer(this.clusters);
         },
-        initLayers: function () { },
+        goTo: function (coords) {
+            this.map.setView(coords, 16);
+        }
     }
 })
